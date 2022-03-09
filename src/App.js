ed0f7a23f1d2ac7040886caafb532e756310440d
semyung oh <semyung@lessbutter.co>
2022-03-10 01:30:48 +0900

    [FEAT] Setup dependencies

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="w-screen h-screen bg-slate-200 flex flex-col justify-center items-center">
+      <p className="text-red-400 font-bold uppercase">welcome to tailwind</p>
+      <p className="text-red-400 font-bold uppercase">
+        setup by semyung oh & taeyoung hwang
+      </p>
+    </main>
   );
 }
 
