7e2d341af8893dc5cd33bcb98249191ec5b571e2
semyung oh <semyung@lessbutter.co>
2022-03-10 21:22:51 +0900

    [FEAT] Create login route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App";
+import Login from "./routes/Login";
 
 import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById("root"),
 );
