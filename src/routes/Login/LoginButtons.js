6c973b6343a075ed04a3a14ffd6d57fb27b13c04
semyung oh <semyung@lessbutter.co>
2022-03-12 16:44:30 +0900

    [UPDATE] 로그인 로직 핸들러 수정

--- src/routes/Login/LoginButtons.js
+++ src/routes/Login/LoginButtons.js
@@ -6,17 +6,17 @@ const GOOGLE_URL = process.env.REACT_APP_AUTH_GOOGLE_URL;
 const KAKAO_URL = process.env.REACT_APP_AUTH_KAKAO_URL;
 
 function LoginButtons() {
-  const curriedAssignHandler = url => () => window.location.assign(url);
+  const moveToSocialLoginPage = url => () => window.location.replace(url);
 
   return (
     <>
-      <button className="mr-4" onClick={curriedAssignHandler(GITHUB_URL)}>
+      <button className="mr-4" onClick={moveToSocialLoginPage(GITHUB_URL)}>
         GITHUB
       </button>
-      <button className="mr-4" onClick={curriedAssignHandler(KAKAO_URL)}>
+      <button className="mr-4" onClick={moveToSocialLoginPage(KAKAO_URL)}>
         KAKAO
       </button>
-      <button className="mr-4" onClick={curriedAssignHandler(GOOGLE_URL)}>
+      <button className="mr-4" onClick={moveToSocialLoginPage(GOOGLE_URL)}>
         GOOGLE
       </button>
       <Outlet />
