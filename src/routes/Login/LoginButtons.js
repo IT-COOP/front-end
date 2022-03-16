29dab59bbf90b623b52eed01911b9437ce75f65f
Taeyoung <hty3466@naver.com>
2022-03-16 10:47:16 +0900

    [UPDATE] 와이어프레임 css수정

--- src/routes/Login/LoginButtons.js
+++ src/routes/Login/LoginButtons.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { GoogleIcon, KakaoIcon, GitHubIcon } from '../../assets/icons/socialLogin';
-import { Close } from '../../assets/icons';
+import {
+  GoogleIcon,
+  KakaoIcon,
+  GitHubIcon,
+} from "../../assets/icons/socialLogin";
+import { Close } from "../../assets/icons";
 
 const GITHUB_URL = process.env.REACT_APP_AUTH_GITHUB_URL;
 const GOOGLE_URL = process.env.REACT_APP_AUTH_GOOGLE_URL;
@@ -10,42 +14,51 @@ const KAKAO_URL = process.env.REACT_APP_AUTH_KAKAO_URL;
 function LoginButtons() {
   const moveToSocialLoginPage = url => () => window.location.replace(url);
 
-  const buttonCss = `relative mb-4 p-4 sm:w-[300px] sm:h-[45px] rounded-[5px] font-bold text-[14px] mb-[10px]`
-  const buttonIconCss = `absolute`
-  const loginTitleCss = `text-[30px] mb-[32px] font-bold text-center sm:text-[20px] sm:mb-[40px]`
+  const buttonCss = `relative mb-4 p-4 sm:w-[100%] sm:h-[45px] rounded-[5px] font-bold text-[14px] mb-[10px] shadow-md`;
+  const buttonIconCss = `absolute`;
+  const loginTitleCss = `text-[30px] mb-[32px] font-bold text-center sm:text-[20px] sm:mb-[40px]`;
   return (
-  <section className='fixed w-screen h-screen bg-black/70 flex justify-center items-center'>
-    <section className="flex relative flex-col w-[800px] h-[570px] justify-center items-center sm:w-[337px] sm:h-[392px] bg-[#F9F9F9]  rounded-xl">
-        <button className='absolute right-4 top-4'>
-          <Close/>
+    <section className="fixed w-screen h-screen bg-black/70 flex justify-center items-center sm:px-[20px]">
+      <section className="flex relative flex-col bg-[#F9F9F9] rounded-[15px] w-[800px] h-[570px] justify-center items-center sm:w-screen sm:h-[392px] sm:px-[10px]">
+        <button className="absolute right-4 top-4">
+          <Close />
         </button>
-      <ul className='flex flex-col'>
-        <li>
-          <h1 className={`${loginTitleCss}`}>
-              IT COOP에 오신것을 환영합니다!
-          </h1>
-          <p className='text-[14px] mb-[40px] text-center sm:mb-[31px]'>
-            로그인이 필요한 서비스 입니다.
-          </p>
-        </li>
-        <li className='flex flex-col'>
-          <button className={`${buttonCss} bg-[#FEE500]`} onClick={moveToSocialLoginPage(KAKAO_URL)}>
-            <KakaoIcon className={buttonIconCss}/>
-            카카오로 시작하기
-          </button>
-          <button className={`${buttonCss} bg-white`} onClick={moveToSocialLoginPage(GOOGLE_URL)}>
-            <GoogleIcon className='absolute'/>
-            구글로 시작하기
-          </button>
-          <button className={`${buttonCss} bg-[#242424] text-white`} onClick={moveToSocialLoginPage(GITHUB_URL)}>
-            <GitHubIcon className={buttonIconCss}/>
-            GitHub로 시작하기
-          </button>
-        </li>
-      </ul>      
-      <Outlet/>
+        <ul className="flex flex-col w-[100%]">
+          <li>
+            <h1 className={`${loginTitleCss}`}>
+              <span className="sm:block">IT COOP에</span> 오신것을 환영합니다!
+            </h1>
+            <p className="text-[14px] mb-[40px] text-center sm:mb-[31px]">
+              로그인이 필요한 서비스 입니다.
+            </p>
+          </li>
+          <li className="flex flex-col">
+            <button
+              className={`${buttonCss} bg-[#FEE500]`}
+              onClick={moveToSocialLoginPage(KAKAO_URL)}
+            >
+              <KakaoIcon className={buttonIconCss} />
+              카카오로 시작하기
+            </button>
+            <button
+              className={`${buttonCss} bg-white`}
+              onClick={moveToSocialLoginPage(GOOGLE_URL)}
+            >
+              <GoogleIcon className={buttonIconCss} />
+              구글로 시작하기
+            </button>
+            <button
+              className={`${buttonCss} bg-[#242424] text-white`}
+              onClick={moveToSocialLoginPage(GITHUB_URL)}
+            >
+              <GitHubIcon className={buttonIconCss} />
+              GitHub로 시작하기
+            </button>
+          </li>
+        </ul>
+        <Outlet />
+      </section>
     </section>
-  </section>
   );
 }
 
--- src/state/apis/userApi.js
+++ src/state/apis/userApi.js
@@ -1,31 +1,27 @@
-import { instance } from './axiosInstance';
+import { instance } from "./axiosInstance";
 
-
-export const userApis ={
+export const userApis = {
   // 토큰 확인해서 회원가입한 사람이면 유저 정보 가져오고 아니면 빈 스트링 받음
-  getUser : token =>
-    instance.get("login/validation",{
-      headers : {
-        authorization :token
-      }}
-    ),
-
-  //최초 로그인이나, 유저정보 튜토리얼 완성 안한 사람들 
-  createUser : ({ data, token }) => 
-    instance.post("login/completion",data,{
-      headers : {
-        authorization : token
-      }
-    }
-    ),
-  
-  //유저 데이터 수정 
-  userProfileEdit : data =>
-    instance.post("api/",{
-      data
+  getUser: token =>
+    instance.get("login/validation", {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
     }),
 
-  
-} 
+  //최초 로그인이나, 유저정보 튜토리얼 완성 안한 사람들
+  createUser: ({ data, token }) =>
+    instance.post("login/completion", data, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    }),
 
-export default instance
+  //유저 데이터 수정
+  userProfileEdit: data =>
+    instance.post("api/", {
+      data,
+    }),
+};
+
+export default instance;
