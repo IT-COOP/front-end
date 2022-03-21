4ed93d8467680365a30b0dc0f909f26998962a4a
Taeyoung <hty3466@naver.com>
2022-03-21 20:49:08 +0900

    [UPDATE]react-select 라이브러리 추가

--- src/index.js
+++ src/index.js
@@ -8,28 +8,25 @@ import RecruitWrite from "./components/recruit/RecruitWrite";
 import Recruit from "./routes/recruit/Recuit";
 import Header from "./components/header/Header";
 import { Provider } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import App from "./App";
 import { store } from "./state/configureStore";
+import MyPage from "./routes/MyPage";
 
 import "./index.css";
-import MyPage from "./routes/MyPage";
 
 ReactDOM.render(
   <BrowserRouter>
-  <Provider store={store}>
-    <main className="max-w-[1224px] mx-[auto] pt-[100px]">
-      <Header />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/recruit" element={<Recruit />} />
-        <Route path="/recruit/write/:id" element={<RecruitWrite />} />
-        <Route path="/recruit/:id" element={<RecruitBoardDetail />} />
-      </Routes>
-    </main>
-  </Provider>
+    <Provider store={store}>
+      <main className="max-w-[1224px] mx-[auto] pt-[100px]">
+        <Header />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/recruit" element={<Recruit />} />
+          <Route path="/recruit/write/:id" element={<RecruitWrite />} />
+          <Route path="/recruit/:id" element={<RecruitBoardDetail />} />
+        </Routes>
+      </main>
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root"),
 );
