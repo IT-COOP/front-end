e68ab252dd45735fd2ad362f4a8a41fb4b9552b4
semyung oh <semyung@lessbutter.co>
2022-04-02 02:45:01 +0900

    [UPDATE] 모집중 관련 툴팁 구현

--- src/routes/User/views/UserProjects.js
+++ src/routes/User/views/UserProjects.js
@@ -31,6 +31,7 @@ const IconCollectionByName = {
 };
 
 function UserProjects({ isCurrentUserPage, userId }) {
+  const [isTooltipHovered, setIsTooltipHovered] = useState(false);
   const navigate = useNavigate();
 
   const tabList = isCurrentUserPage
@@ -51,6 +52,8 @@ function UserProjects({ isCurrentUserPage, userId }) {
   if (isLoading) {
     return <div className="w-full h-[218px] bg-white2 animate-pulse" />;
   }
+
+  console.log(projectList);
 
   return (
     <div>
@@ -90,23 +93,23 @@ function UserProjects({ isCurrentUserPage, userId }) {
 
               const lastUpsertedDate =
                 !updatedAt || createdAt === updatedAt ? createdAt : updatedAt;
-
               const parsedUpsertText = convertDateText(lastUpsertedDate);
-              const [applyInformation] = recruitApplies;
 
               return (
                 <li
-                  onClick={moveToRecruitDetailPage(post.recruitPostId)}
                   key={post.recruitPostId}
                   className={classNames(
-                    "text-[15px] mb-[45px] last:mb-0 cursor-pointer border-b border-b-gray2 h-[100px]",
+                    "text-[15px] mb-[45px] last:mb-0  border-b border-b-gray2",
                     {
-                      "flex justify-between": applyInformation !== undefined,
+                      "flex justify-between": recruitApplies.length === 1,
                     },
                   )}
                 >
                   <div>
-                    <div className="flex items-center">
+                    <div
+                      onClick={moveToRecruitDetailPage(post.recruitPostId)}
+                      className="flex items-center"
+                    >
                       <span className="inline-block mr-[3px]">
                         {post.title}
                       </span>
@@ -116,9 +119,88 @@ function UserProjects({ isCurrentUserPage, userId }) {
                       {parsedUpsertText} | {post.author2.nickname}
                     </span>
                   </div>
-                  {Boolean(applyInformation) && (
-                    <CancelApplyTooltop info={applyInformation} />
-                  )}
+                  {DEFAULT_TAB_LIST[activeIndex].name === "모집중" &&
+                    projectList?.length > 0 && (
+                      <div
+                        onMouseEnter={() => setIsTooltipHovered(true)}
+                        className="pl-[4px] mt-[8px] mb-[31px] flex gap-x-[6px] items-center w-max relative"
+                      >
+                        <div>
+                          {recruitApplies?.map(information => (
+                            <img
+                              className="w-[25px] h-[25px] rounded-full inline-block mr-[3px]"
+                              src={information.applicant2.profileImgUrl}
+                              alt={`${information.applicant2.nickname}'s profile`}
+                              key={information.applicant}
+                            />
+                          ))}
+                        </div>
+                        <div className="text-[15px]">
+                          <span className="text-blue3">
+                            {recruitApplies?.length}명
+                          </span>
+                          <span>의 신청자가 있어요!</span>
+                        </div>
+                        <div
+                          onMouseLeave={() => setIsTooltipHovered(false)}
+                          className={classNames(
+                            "absolute h-auto top-[125%] left-1/4 w-max bg-white flex justify-center items-center border border-gray5 rounded-[9px]",
+                            {
+                              hidden: !isTooltipHovered,
+                              block: isTooltipHovered,
+                            },
+                          )}
+                        >
+                          <div className="absolute bottom-full border-[5px] border-b-[10px] border-t-transparent border-r-transparent border-b-white border-l-transparent z-10"></div>
+                          <div className="absolute bottom-[100%] border-[6px] border-b-[12px] border-t-transparent border-r-transparent border-b-gray5 border-l-transparent"></div>
+                          <ul>
+                            {recruitApplies.map(
+                              (information, index, { length }) => {
+                                const handleNameClick = e => {
+                                  e.stopPropagation();
+                                  navigate(`/user/${information.applicant}`);
+                                };
+                                return (
+                                  <React.Fragment key={information.applicant}>
+                                    <li className="px-[20px] py-[13px] my-[9px] min-h-[66px]">
+                                      <div>
+                                        <div
+                                          onClick={handleNameClick}
+                                          className="text-[15px] mb-[5px] cursor-pointer"
+                                        >
+                                          <span className="underline text-blue3">
+                                            {information.applicant2.nickname}
+                                          </span>
+                                          <span> 님이 신청했어요</span>
+                                        </div>
+                                        <span className="text-[13px] text-gray4">
+                                          {convertDateText(
+                                            information.createdAt,
+                                          )}
+                                        </span>
+                                      </div>
+                                    </li>
+                                    {index + 1 === length ? null : (
+                                      <div className="w-auto mx-[7px] h-px bg-gray3" />
+                                    )}
+                                  </React.Fragment>
+                                );
+                              },
+                            )}
+                            <li
+                              role="button"
+                              className="h-[67px] w-[101%] relative -left-[0.5%] -bottom-[0.5%] flex justify-center items-center bg-blue3 text-white text-[17px] font-bold rounded-b-[8px]"
+                            >
+                              신청자 명단 보러가기
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    )}
+                  {DEFAULT_TAB_LIST[activeIndex].name === "신청중" &&
+                    recruitApplies?.length > 0 && (
+                      <CancelApplyTooltop info={recruitApplies[0]} />
+                    )}
                 </li>
               );
             })}
