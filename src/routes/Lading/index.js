a16fa009027d3120a0ac3a7f9676aa8cae8b1f61
Taeyoung <hty3466@naver.com>
2022-04-05 12:12:42 +0900

    [UPDATE] 랜딩페이지 이미지사이즈변경

--- src/routes/Lading/index.js
+++ src/routes/Lading/index.js
@@ -6,6 +6,7 @@ const LandingPage = () => {
       <img
         src="https://s3.ap-northeast-2.amazonaws.com/it-coop.co.kr/images/Landing.png"
         alt="랜딩 이미지"
+        className="w-screen min-w-[1224px]"
       />
     </section>
   );
