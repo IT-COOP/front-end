aa1f7d4a192cb8249e3e8886c3b655541a6662ae
Taeyoung <hty3466@naver.com>
2022-04-05 18:41:16 +0900

    [FIX] 클라우드 프론트 설정

--- src/components/recruit/recruitWriteView/Content.js
+++ src/components/recruit/recruitWriteView/Content.js
@@ -3,13 +3,15 @@ import React, { useState } from "react";
 const Content = ({ handleContent }) => {
   const [boardContent, setBoardContent] = useState();
   const handleBoardContent = onChange => {
+    console.log(onChange.target.value.split("\n").filter(text => text !== ""));
     setBoardContent(onChange.target.value);
     handleContent(onChange.target.value);
   };
-
+  console.log(boardContent);
   return (
-    <li className="flex pb-[60px]">
+    <li className="flex flex-col pb-[60px]">
       <p className="w-[208px] text-[17px]"> 내용 </p>
+      <p>{boardContent?.split("\n").filter(text => text !== "")}</p>
       <textarea
         className="flex-1 h-[315px] border border-gray3 resize-none p-[4px]"
         type="text"
