8e492dba29bd55392a7aaaa69d6976cfd2432f84
Taeyoung <hty3466@naver.com>
2022-04-08 02:19:02 +0900

    [UPDATE] 기본이미지 url 주소 변경

--- src/constants/defaultImages.js
+++ src/constants/defaultImages.js
@@ -1,14 +1,14 @@
 export const recruitBoardDefaultUrl =
-  "https://d1s2lg1ngj5ops.cloudfront.net/images/boardDefaultImg.png";
+  "https://dvmsryszvotxl.cloudfront.net/images/boardDefaultImg.png";
 
 export const feDefaultImgUrl =
-  "https://d1s2lg1ngj5ops.cloudfront.net/images/fe.png";
+  "https://dvmsryszvotxl.cloudfront.net/images/fe.png";
 
 export const beDefaultImgUrl =
-  "https://d1s2lg1ngj5ops.cloudfront.net/images/be.png";
+  "https://dvmsryszvotxl.cloudfront.net/images/be.png";
 
 export const deDefaultImgUrl =
-  "https://d1s2lg1ngj5ops.cloudfront.net/images/de.png";
+  "https://dvmsryszvotxl.cloudfront.net/images/de.png";
 
 export const plDefaultImgUrl =
-  "https://d1s2lg1ngj5ops.cloudfront.net/images/pl.png";
+  "https://dvmsryszvotxl.cloudfront.net/images/pl.png";
