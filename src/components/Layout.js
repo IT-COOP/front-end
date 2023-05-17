2f083b07dd9b7e73435f8edb2e92ac3d1382b96a
h2oterry <127275174+h2oterry@users.noreply.github.com>
2023-05-17 21:09:40 +0900

    test

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom";
 
 import Header from "./header/Header";
 import usePageLocateScrollTop from "../hooks/usePageLocateScrollTop";
-
 function Layout() {
   usePageLocateScrollTop();
+  console.log(1234);
   return (
     <>
       <Header />
